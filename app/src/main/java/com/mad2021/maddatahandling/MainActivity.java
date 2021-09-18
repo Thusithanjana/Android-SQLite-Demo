3997706aa2da26080fed498583afee1ee87f3625
Thusithanjana Thilakarathana <thusithanjana@gmail.com>
2021-09-18 12:43:33 +0530

    Complete Product

--- app/src/main/java/com/mad2021/maddatahandling/MainActivity.java
+++ app/src/main/java/com/mad2021/maddatahandling/MainActivity.java
@@ -1,13 +1,19 @@
 package com.mad2021.maddatahandling;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.mad2021.maddatahandling.database.DBHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,11 +44,77 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "Something went wrong ;(", Toast.LENGTH_SHORT).show();
             }
 
-
-
-
         }
 
+    }
+
+    public void viewAll(View view){
+        DBHelper dbHelper = new DBHelper(this);
+
+        List info = dbHelper.readAllInfo();
+        List items = new ArrayList<Integer>();
+
+        String[] data = (String[]) info.toArray(new String[0]);
+
+        //Toast.makeText(this, info.toString(), Toast.LENGTH_SHORT).show();
+
+//        Snackbar snackbar = Snackbar.make(view, info.toString(),Snackbar.LENGTH_LONG);
+//        snackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
+//        snackbar.show();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Users");
+        builder.setItems(data, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String userName = data[i].split(":")[0];
+                edtName.setText(userName);
+                edtPassword.setText("*************************");
+
+            }
+        });
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
+
+    public void deleteUser(View view){
+        DBHelper dbHelper = new DBHelper(this);
+
+        String userName = edtName.getText().toString();
+
+        if(userName.isEmpty()){
+            Toast.makeText(this, "Please select user to Delete", Toast.LENGTH_SHORT).show();
+        }else{
+            dbHelper.deleteInfo(userName);
+            Toast.makeText(this, "User deleted!", Toast.LENGTH_SHORT).show();
+
+            edtName.setText("");
+            edtPassword.setText("");
+        }
+
+    }
+
+    public void updateUser(View view){
+        DBHelper dbHelper = new DBHelper(this);
+        String userName = edtName.getText().toString();
+        String password = edtPassword.getText().toString();
+
+        if(userName.isEmpty()||password.isEmpty()){
+
+        }else{
+            dbHelper.updateInfo(view,userName,password);
+        }
+
+    }
+
+
+
 }
--- app/src/main/java/com/mad2021/maddatahandling/database/DBHelper.java
+++ app/src/main/java/com/mad2021/maddatahandling/database/DBHelper.java
@@ -2,10 +2,19 @@ package com.mad2021.maddatahandling.database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.mad2021.maddatahandling.MainActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -34,6 +43,75 @@ public class DBHelper extends SQLiteOpenHelper {
         values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD, password);
 
         return db.insert(UsersMaster.Users.TABLE_NAME, null, values);
+    }
+
+    // new code
+    public List readAllInfo(){
+        SQLiteDatabase db = getReadableDatabase();
+
+        String [] projection = {
+                UsersMaster.Users._ID,
+                UsersMaster.Users.COLUMN_NAME_USERNAME,
+                UsersMaster.Users.COLUMN_NAME_PASSWORD
+        };
+
+        String sortOrder = UsersMaster.Users.COLUMN_NAME_USERNAME + " DESC";
+
+        Cursor cursor = db.query(
+                UsersMaster.Users.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        List info = new ArrayList<>();
+
+        while (cursor.moveToNext()){
+            String userName = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_USERNAME));
+            String password = cursor.getString(cursor.getColumnIndexOrThrow(UsersMaster.Users.COLUMN_NAME_PASSWORD));
+
+            info.add(userName+": "+password);
+        }
+        cursor.close();
+
+        return info;
+    }
+
+    public void deleteInfo(String userName){
+        SQLiteDatabase db = getReadableDatabase();
+        String selection = UsersMaster.Users.COLUMN_NAME_USERNAME + " LIKE ?";
+        String [] selectionArgs = { userName };
+        db.delete(UsersMaster.Users.TABLE_NAME,selection,selectionArgs);
+
+
+    }
+
+    public void updateInfo(View view, String userName, String password){
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+
+        values.put(UsersMaster.Users.COLUMN_NAME_PASSWORD, password);
+
+        String selection = UsersMaster.Users.COLUMN_NAME_USERNAME + " Like ?";
+        String[] selectionArgs = {userName};
+
+        int count = db.update(
+                UsersMaster.Users.TABLE_NAME,
+                values,
+                selection,
+                selectionArgs
+        );
+
+        Snackbar snackbar = Snackbar.make(view, count + " rows effected",Snackbar.LENGTH_LONG);
+        snackbar.setAnimationMode(Snackbar.ANIMATION_MODE_SLIDE);
+        snackbar.show();
+
+
+
     }
 
     @Override
